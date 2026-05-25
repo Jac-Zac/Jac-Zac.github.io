e1ec36ed3f846c2584e3f938670198ffdbeac901
Jac-Zac <59306950+Jac-Zac@users.noreply.github.com>
2026-05-25 11:24:33 +0000

    Deploying to gh-pages from @ Jac-Zac/Jac-Zac.github.io@2fa1d389bb11b74a81a87e85fe950a7c73cb6fb3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Master&#39;s student in Data Science &amp; AI · Mechanistic interpretability, HPC, computer vision. CV rendered with RenderCV from _data/cv.yml.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
